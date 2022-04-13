d62ce2aabc732c80dfa3a995a0f14e7c59bd8458
Valeri Sabev <valchygaming@gmail.com>
2022-04-13 02:27:07 +0200

    bug fix of " ' "

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Wordle() {
 	return (
 		<div className="text-center bg-neutral-900 h-screen w-screen">
-			<h1 className='text-3xl font-bold text-slate-300 py-4'>Valeri's Wordle</h1>
+			<h1 className='text-3xl font-bold text-slate-300 py-4'>My Wordle</h1>
 		</div>
 	);
 }
